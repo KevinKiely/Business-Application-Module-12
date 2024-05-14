d8e2e7aae49769088612b8adafe58046924499cd
KevinKiely <Kevinkiely762@gmail.com>
2024-05-14 15:11:59 -0400

    Setting up file structure, installing dependencies

---
+++ server.js
@@ -0,0 +1,40 @@
+// Dependencies 
+const express = require('express');
+const mysql = require('mysql2');
+const inquirer = require ('inquirer');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'Kilimanjaro1776',
+    database: 'books_db'
+  },
+  console.log(`Connected to the books_db database.`)
+);
+
+// Questions to be asked by inquirerer to user
+const questions = [
+
+];
+
+inquirer.prompt(questions);
+
+
+// Catch
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+// Port listening at localhost3001 or designated process.env
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
